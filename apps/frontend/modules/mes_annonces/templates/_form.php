ce42c04a7154ffe1ff2a7e6c506cf7516ef93b29
obarbier <obarbier@me.com>
2011-03-16 15:32:14 +0000

    suite épuration

--- apps/frontend/modules/mes_annonces/templates/_form.php
+++
@@ -1,2 +0,0 @@
-<form method="POST" action="<?php url_for( 'listing/filter' ); ?>"><input type="text" id="annonce_titre"
-	name="annonce[titre][text]" value=""> <input type="submit" value="rechercher" /></form>
